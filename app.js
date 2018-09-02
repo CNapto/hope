d630144173fffa04f9a52a78951f0470c79f421c
angad <angadsharma1016@gmail.com>
2018-09-02 22:49:41 +0530

    socket connection

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const socket = require("socket.io");
 const app = express();
+
 
 app.set("view engine","ejs");
 app.use(express.static("static"));
-
 
 app.get("/",(req,res)=>{
     res.render("index");
 });
 
 
-app.listen(3000,"0.0.0.0",()=>console.log("Listening...."));
+let server = app.listen(3000,"0.0.0.0",()=>console.log("Listening...."));
+
+let io = socket(server);
+
+io.sockets.on("connection",(client)=>{
+    client.emit("base station",{message:"we are coming to save you"});
+    client.on("reply",(data)=>{
+        console.log(data);
+    });
+}); 
